605916488956961d874d4252c8dda7d9eb98a2b5
paulsilm <paul.silm99@gmail.com>
2021-12-03 23:18:13 +0200

    Finish day 3

---
+++ 3b.py
@@ -0,0 +1,34 @@
+import helper
+import numpy as np
+from statistics import multimode
+
+def task2():
+  ns = helper.get_int_input_for_file("3")
+  binmatrix = np.array([toDigits(n) for n in ns])
+  o2Rating = calculateRating(binmatrix.copy())
+  co2Rating = calculateRating(binmatrix.copy(), 1)
+  print(o2Rating*co2Rating)
+
+# overengineered function
+def calculateRating(matrix, thing=0):
+  for i in range(12):
+    temp = matrix[:,i]
+    # if cnt(1) == cnt(0): m = 1
+    m = sorted(multimode(temp), reverse=True)[0]
+    if thing: # reverse value for co2Rating
+      m = 1 - m
+    matrix = matrix[temp == m] # filter stuff
+    if len(matrix) == 1:
+      return toInt(matrix[0])
+
+def toDigits(d:int):
+  return np.array([(d // 10**(11-i)) % 10 for i in range(12)])
+
+def toInt(d):
+  b:int = 0
+  for i in range(12):
+    b += (d[i])*2**(11-i)
+  return b
+
+if __name__ == '__main__':
+  task2()
